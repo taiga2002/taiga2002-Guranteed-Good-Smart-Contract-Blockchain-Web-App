659530ff9950d3c8e9f31168d557cf3343871281
Taiga Kitao <taiga20022002@gmail.com>
2022-10-16 10:31:00 -0700

    finished connecting to the project specification

--- pages/projects/show.js
+++ pages/projects/show.js
@@ -5,6 +5,8 @@ import Project from "../../ethereum/project";
 import web3 from "../../ethereum/web3";
 import ContributeForm from "../../components/ContributeForm";
 import { Link } from "../../routes";
+import ProjectSpec from "../../components/ProjectSpec";
+import NavBar from "../../components/NavBar";
 
 class ProjectShow extends Component {
   static async getInitialProps(props) {
@@ -23,76 +25,51 @@ class ProjectShow extends Component {
       image: summary[7],
     };
   }
-
-  renderCards() {
+  render() {
     const {
-      balance,
-      manager,
+      address,
       minimumContribution,
+      balance,
       requestsCount,
       approversCount,
+      manager,
+      name,
+      description,
+      image,
     } = this.props;
-
-    const items = [
-      {
-        header: manager,
-        meta: "Address of Manager",
-        description:
-          "The manager created this project and can create requests to withdraw money",
-        style: { overflowWrap: "break-word" },
-      },
-      {
-        header: minimumContribution,
-        meta: "Minimum Contribution (wei)",
-        description:
-          "You must contribute at least this much wei to become an approver",
-      },
-      {
-        header: requestsCount,
-        meta: "Number of Requests",
-        description:
-          "A request tries to withdraw money from the contract. Requests must be approved by approvers",
-      },
-      {
-        header: approversCount,
-        meta: "Number of Approvers",
-        description:
-          "Number of people who have already donated to this project",
-      },
-      {
-        header: web3.utils.fromWei(balance, "ether"),
-        meta: "Project Balance (ether)",
-        description:
-          "The balance is how much money this project has left to spend.",
-      },
-    ];
-
-    return <Card.Group items={items} />;
-  }
-
-  render() {
     return (
-      <Layout>
-        <h3>Project Show</h3>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
-            <Grid.Column width={6}>
-              <ContributeForm address={this.props.address} />
-            </Grid.Column>
-          </Grid.Row>
+      // <Layout>
+      //   <h3>Project Show</h3>
+      //   <Grid>
+      //     <Grid.Row>
+      //       <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
+      //       <Grid.Column width={6}>
+      //         <ContributeForm address={this.props.address} />
+      //       </Grid.Column>
+      //     </Grid.Row>
 
-          <Grid.Row>
-            <Grid.Column>
-              <Link route={`/projects/${this.props.address}/requests`}>
-                <a>
-                  <Button primary>View Requests</Button>
-                </a>
-              </Link>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Layout>
+      //     <Grid.Row>
+      //       <Grid.Column>
+      //         <Link route={`/projects/${this.props.address}/requests`}>
+      //           <a>
+      //             <Button primary>View Requests</Button>
+      //           </a>
+      //         </Link>
+      //       </Grid.Column>
+      //     </Grid.Row>
+      //   </Grid>
+      // </Layout>
+      <>
+        <NavBar />
+        <ProjectSpec
+          image={image}
+          name={name}
+          mini={minimumContribution}
+          approvals={approversCount}
+          manager={manager}
+          description={description}
+        />
+      </>
     );
   }
 }
