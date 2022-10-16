fe43a0b4502e8e1a14eabb983bc7505ac27ce140
Joying Yang <joying@berkeley.edu>
2022-10-16 09:08:34 -0700

    Minor fixes

--- components/Choices.js
+++ components/Choices.js
@@ -8,7 +8,7 @@ export default function Choices() {
                             <div className="w-10 mx-auto">
                                 <img
                                     className="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full"
-                                    src="engineer.png"
+                                    src="management.png"
                                     alt=""
                                 />
                             </div>
@@ -16,7 +16,7 @@ export default function Choices() {
                         <div className="mx-auto">
                             <h4 className="text-lg font-semibold">Manage</h4>
                         </div>
-                        <div className="mx-auto text-center px-10 text-sm font-thin">Pee pee poo among us among us help me shart shart shart</div>
+                        <div className="mx-auto text-center px-10 text-sm font-thin">Begin getting the funds you need for the projects you need.</div>
                         <div className="mx-auto text-sm text-orange-400">Learn more.</div>
                     </div>
                     <div className="grid grid-cols-1 place-content-center justify-center content-center mx-auto px-8 space-y-2">
@@ -24,7 +24,7 @@ export default function Choices() {
                             <div className="w-10 mx-auto">
                                 <img
                                     className="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full"
-                                    src="engineer.png"
+                                    src="hand.png"
                                     alt=""
                                 />
                             </div>
@@ -32,7 +32,7 @@ export default function Choices() {
                         <div className="mx-auto">
                             <h4 className="text-lg font-semibold">Invest</h4>
                         </div>
-                        <div className="mx-auto text-center px-10 text-sm font-thin">Pee pee poo among us among us help me shart shart shart</div>
+                        <div className="mx-auto text-center px-10 text-sm font-thin">Help support the NPO you believe in achieve their goal.</div>
                         <div className="mx-auto text-sm text-orange-400">Learn more.</div>
                     </div>
                     <div className="grid grid-cols-1 place-content-center justify-center content-center mx-auto px-8 space-y-2">
@@ -48,7 +48,7 @@ export default function Choices() {
                         <div className="mx-auto">
                             <h4 className="text-lg font-semibold">Apply</h4>
                         </div>
-                        <div className="mx-auto text-center px-10 text-sm font-thin">Pee pee poo among us among us help me shart shart shart</div>
+                        <div className="mx-auto text-center px-10 text-sm font-thin">Offer your skillset to a project and people that need your expertise.</div>
                         <div className="mx-auto text-sm text-orange-400">Learn more.</div>
                     </div>
                 </div>
