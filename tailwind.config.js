7ca24d31e03b3e2563320ddd199b2b332f6e9494
Eugene Chao <epchao@berkeley.edu>
2022-10-16 00:27:22 -0700

    reinitialize tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,61 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+// THIS OBJECT SHOULD BE SIMILAR TO ./helpers/theme.js
+const themeConstants = {
+  paper: "#F9F9F9",
+  primary: {
+    main: "#fff",
+    dark: "#e5e5e5",
+  },
+  secondary: {
+    main: "#212121",
+    dark: "#3A3A3A",
+  },
+  error: {
+    main: "#b22222",
+    dark: "#8b0000",
+  },
+  fg: { main: "#fff", dark: "rgba(55, 65, 81, 1)" },
+  breakpoints: {
+    xs: "0px",
+    mb: "350px",
+    sm: "600px",
+    md: "960px",
+    lg: "1280px",
+    xl: "1920px",
+  },
+};
+
+module.exports = withMT({
+  mode: "jit",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        paper: themeConstants.paper,
+        primary: themeConstants.primary,
+        secondary: themeConstants.secondary,
+        error: themeConstants.error,
+        fg: themeConstants.fg.main,
+        "fg-dark": themeConstants.fg.dark,
+      },
+    }, 
+    
+    // We over ride the whole screens with breakpoints for width. The 'ha' breakpoint will help us in blocking hover animations on devices not supporting hover.
+    screens: {
+      ...defaultTheme.screens,
+      ...themeConstants.breakpoints,
+      ha: { raw: "(hover: hover)" },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+});
